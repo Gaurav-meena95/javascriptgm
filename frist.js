05f236352587b8f25efebd04505c2368bc8f858e
Gaurav Meena <meenajigaurav9589@gmail.com>
2024-11-26 09:02:15 +0530

    first commit

---
+++ frist.js
@@ -0,0 +1,456 @@
+// full_name='Amisha namdev';
+// let a;
+// // const a;
+// age=24;
+// price=99.3
+// let x = BigInt('234')
+// console.log(full_name);
+// console.log(age);
+// console.log(price);
+// console.log(x);
+// console.log(a);
+// console.log(typeof x);
+
+// const student = {
+//     full_name:'gaurav Meena',
+//     age:20,cgpa:9.9,
+//     ispass:true
+// }; 
+// student['age'] = student['age']+1;
+// // const name = 'Amisha';
+// // name='Gaurav';
+
+// student['full_name']='Amisha';
+
+// console.log(student['cgpa ']);
+
+// const product={
+//     name:"Ball pen",
+//     rating: 4,
+//     offer:5,
+//     price:80,
+// };
+// console.log(product);
+
+// const profile={
+//     username:'Gaurav Meena',
+//     Post:193,
+//     isfollow:true,
+//     Follower:894,
+//     following:54,
+//     about:'cricket lover ,son of farmer',
+// };
+// console.log(profile);
+ 
+// let a=6;
+// let b=5;
+// a+=4
+// console.log("a :",a)
+// a-=4
+// console.log("a :",a)
+// a*=5  
+// console.log("a :",a)
+// a/=5
+// console.log("a :",a)
+// a//=5
+// console.log("a :",a)
+// a%=5
+// console.log("a :",a)
+// a**=5
+// console.log("a :",a)
+// a+=5
+// console.log("a :",a)
+
+// let cond=a>b;
+// let cond2=a==5;
+// console.log(cond && cond2)
+// console.log(cond || cond2)
+// console.log(!(a<b) )
+// console.log(a===b)
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a!==b)
+
+
+
+
+// let age=8;
+// if (age >=18){
+//     console.log('You can Vote')
+// }
+// else {
+//     console.log("you cannot vote")   
+// }
+
+
+
+// let num=9;
+// if (num%2==0){
+//     console.log('You can Vote')
+// }
+// else {
+//     console.log("you cannot vote")   
+// }
+
+
+
+// let age=70;  
+// if (age <18){
+//     console.log('You can Vote')
+// }
+// else if (age>60) {
+//     console.log("you cannot vote")   
+// }
+// else {
+//     console.log("you are in middle vote")   
+// }
+
+// let age=17;
+// let result= age>=18 ? 'adult':'not adult';
+// console.log(result)
+
+// let num= prompt("enter a no:");
+// if (num%5===0){
+//     console.log('Num is multiple of 5')
+// }
+// else{
+
+//     console.log('Num is not  multiple of 5')
+// }
+
+
+
+// let score=prompt('enter No:')
+// let grade;
+// if(score>=80 && score<=100){
+//     grade="A"
+// }else if(score>=70 && score<=79){
+//     grade="B"
+// }else if(score>=60 && score<=69){
+//     grade="C"
+// }else if(score>=50 && score<=59){
+//     grade="D"
+// }else if(score>=0 && score<=49){
+//     grade="F"
+// }
+// console.log('according to your :',grade)
+
+
+
+
+// for (let i=1;i<=10000;i++){
+//     console.log('Gaurav Amisha');
+// }
+// const id=Symbol('123')
+// const ajsdid=Symbol('123')
+
+// console.log(id==ajsdid)
+
+
+// let name1 = 'Gaurav meena' 
+// let name2 = name1
+// name2 = 'Amisha namdev'
+
+// console.log(name2)
+// console.log(name1)
+
+// const name1 = "gaurav Meena"
+// const repooo = 50
+// // console.log(name1+repooo+"Amisha")
+// console.log(`hello my name is ${name1} and my repo cont is ${repooo}`)
+
+// const bbyname = new String('Amisha Namdev')
+// console.log(bbyname)
+// console.log(bbyname.length)
+// console.log(bbyname.toUpperCase())
+// console.log(bbyname.charAt(7))
+// console.log(bbyname.indexOf("g"))
+
+// const newstring = bbyname.substring(0,10,2)
+// console.log(newstring)
+
+
+// const anotherstring = bbyname.slice(-1,43)
+// console.log(anotherstring)
+
+
+// const newstringone = "            GAurav Meena     "
+// console.log(newstringone)
+// console.log(newstringone.trim())
+
+
+
+
+// const url =  'https://gau-rav-meena.com/-hite-sh%20chudu'
+//  console.log(url.replace('%20','gandu'))
+//  console.log(url.includes('gandu'))
+//  console.log(url.split('-'));
+
+
+
+// const sccore  = 400
+// const balance  = new Number(100)
+
+// console.log(sccore);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+
+// const othernumber = 1123.89865
+// console.log(othernumber.toFixed(2));
+// console.log(othernumber.toPrecision(3));
+
+// let hunders  =1000000 
+// console.log(hunders.toLocaleString('en-in'))
+
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.round(4.6))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.6))
+// console.log(Math.min(4,3,6,2,56))
+// console.log(Math.max(4,3,6,2,56))
+
+
+
+
+// console.log(Math.random())
+// console.log(Math.random()*10)
+// console.log(Math.random()*10)
+// console.log(Math.random()*10)
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.random()*(max-min +1)+min)
+// console.log(Math.floor(Math.random()*(max-min +1)+min))
+// console.log(Math.floor(Math.random()*(max-min +1)+min))
+// console.log(Math.floor(Math.random()*(max-min +1)+min))
+// console.log(Math.floor(Math.random()*(max-min +1)+min))
+// console.log(Math.floor(Math.random()*(max-min +1)+min))
+
+
+
+//   let dmyate = new Date()
+//   console.log(dmyate.toString());
+//   console.log(dmyate.toDateString());
+//   console.log(dmyate.toISOString());
+//   console.log(dmyate.toJSON());
+//   console.log(dmyate.toLocaleDateString());
+//   console.log(dmyate.toLocaleString());
+//   console.log(dmyate.toLocaleTimeString());
+//   console.log(dmyate.toTimeString());
+//   console.log(dmyate.toUTCString());
+  
+//   console.log(typeof dmyate);
+
+
+// let newdmyate = new Date(2024,11,30)
+// let newdmyate = new Date(2024,1,30,5,3)
+
+// let newdmyate = new Date('2023-01-31')
+
+// // console.log( newdmyate.toDateString());
+// console.log( newdmyate.toLocaleString());
+
+// let mytimestamp = Date.now()
+// console.log(mytimestamp)
+// console.log(newdmyate.getTime())
+// console.log(Math.floor(Data.now()/1000))
+
+// let newdate = new Date()
+// console.log(newdate);
+// console.log(newdate.getDate());
+// console.log(newdate.getMonth()+1);
+// console.log(newdate.getDay());
+
+// newdate.toLocaleString('default', {
+//     weekday:'long',
+// })
+
+// const  myarry = [0,1,2,3,4,4,5,6,7,7,8,1,23,'Gaurav','Amisha',]
+// const myarry2 = new Array(1,2,3,4,5,6,7)
+// console.log(myarry[14])
+// console.log(myarry)
+// console.log(typeof myarry[13])
+// console.log(typeof myarry[12])
+// console.log(myarry2)
+// console.log(myarry2[5])
+
+
+// myarry.push(2,5,54,6,3,45,3,54)
+// myarry.push(2)
+// myarry.pop()
+
+
+// to add in first
+// myarry.unshift(9)
+
+// myarry.shift()
+// console.log(myarry)
+
+
+
+// console.log(myarry.includes(9))
+// console.log(myarry.includes(2))
+// console.log(myarry.indexOf(5))
+// console.log(myarry.indexOf(9))
+// console.log(myarry.indexOf("Amisha"))
+
+// const newArr = myarry.join()
+
+// console.log(myarry);
+// console.log(typeof newArr);
+
+// console.log('A',  myarry);
+
+// const myn1  = myarry.slice(1,4)
+// console.log(myn1)
+// console.log("B",myarry);
+
+// const myn1  = myarry.splice(1,4)
+// console.log(myn1)
+// console.log("B",myarry);
+
+
+
+// const heros = ['akashy kumar','sunney deol','Gaurav Meena']
+// const frnd = ['golu','ana','adi','rahul']
+// const love = ['Suhani','Amisha']
+
+// heros.push(frnd)
+// console.log(heros[3])
+
+// let b=heros.concat(frnd)
+
+// console.log(heros)
+// console.log(b)
+
+// const allguys = [...heros,...frnd,...love]
+// console.log(allguys)
+
+
+// const dhoka = ['suhani',1,2,6,[4,5,6,],23,45,[34,41,[4,5,6,]]]
+// const my_real = dhoka.flat(Infinity)
+// const my_real = dhoka.flat(1)
+// const my_real = dhoka.flat(2)
+
+// console.log(my_real)
+// console.log(Array.isArray('Gaurav'))
+// console.log(Array.from('Gaurav'))
+// console.log(Array.from({name:'Gaurav'}))
+
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1,score2,score3))
+
+// const mysym = Symbol('key1')
+
+// const jsuser={
+//     name:"Gaurav",
+//     'full name':'Gaurav Amisha',
+//     [mysym]:'myKey1223',
+//     age:18,
+//     email:"gaurav.google.com",
+//     islogdin:false,
+//     lastLoginDays:['monday','saturday']
+// }
+// console.log(jsuser.email) 
+// console.log(jsuser['email'])
+// console.log(jsuser["full name"])
+// console.log(jsuser[mysym])
+// console.log(typeof jsuser.mysym)
+
+// jsuser.email= 'AmishaGaurav@gmaail.com'
+// console.log(jsuser)
+
+// Object.freeze(jsuser)
+
+// jsuser.email= 'AmishaGaurav@gjkfnsdmaail.com'
+// console.log(jsuser);
+
+// jsuser.greeting = function(){
+//     console.log('Hello Meena ji kese ho');
+// }
+// console.log(jsuser.greeting());\
+
+
+// const tinderUse = new Object()
+// const tinderUse = {},
+// tinderUse.id = '123#acv',
+// tinderUse.name = 'AMISHA'  ,
+// tinderUse.islogdin = false
+
+// console.log(tinderUse)
+
+// // const regular = {
+// //     email:'asklfjakjg@kdsfn.com',
+// //     fullname:{
+// //         userFullName:{
+// //             Firstname:'gaurav',
+// //             lastname:'Meena'
+// //         }
+// //     }
+// // }
+// // console.log(regular.fullname.userFullName.Firstname)
+
+
+// const obj1 = {1:'a',2:"b"}
+// const obj2 = {3:'c',4:"d"}
+// const obj4 = {3:'c',4:"d"}
+
+// // const obj3 = {obj1,obj2}
+// // const obj3 = Object.assign(obj1,obj2)
+// // const obj3 = Object.assign({},obj1,obj2,obj4)
+
+// const obj3 = {...obj1,...obj2,...obj4}
+
+// // console.log(obj3);
+
+// const user = [ 
+
+//     {
+//         id:1,
+//         name:"Gaurav",
+//     }
+//     {
+//         id:1,
+//         name:"Gaurav",
+//     }
+//     {
+//         id:1,
+//         name:"Gaurav",
+//     }
+//     {
+//         id:1,
+//         name:"Gaurav",
+//     }
+// ]
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+
+
+// // console.log(user[1].id)
+
+const cousre = {
+    cousrenamew : 'js.hindi',
+    price : '999',
+    cousreInctructor: "meena ji"
+
+}
+const {cousreInctructor:hero} = cousre
+// console.log(cousreInctructor)
+console.log(hero);
